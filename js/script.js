203ed3f3cc73a1a3fff8fcc409dd2dd29b39c381
TawshifTamjidAlam <tawshif27@gmail.com>
2019-04-09 11:49:23 -0700

    Add payment selection functionality

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function inititaliseForm() {
     $('#other-title').hide();
     $('#design').on('input', showColorOptionsForDesign);
     $('.activities').on('input', 'input', handleActivitySelection());
+    $('#payment').on('input', handlePaymentSelection());
 }
 
 function showColorOptionsForDesign(event) {
@@ -33,31 +34,101 @@ function showAndHideOptions($optionsToShow, $optionstoHide) {
 function handleActivitySelection() {
     const $events9To12 = $("input[name = 'js-frameworks'], input[name = 'express'], input[name = 'build-tools']");
     const $events1To4 = $("input[name = 'js-libs'], input[name = 'node'], input[name = 'npm']");
+    const $totalContainer = initialiseTotalCounter();
     return function (event) {
         const $changedEvent = $(event.target);
         const isChecked = $changedEvent.prop('checked');
         const $conflictingEvents = $events9To12.is($changedEvent) ? $events9To12 : $events1To4;
 
-        if(isChecked && $changedEvent.attr('name') !== 'all') {
-            disableConflictingEvents($changedEvent, $conflictingEvents)
-        }else {
+        if (isChecked) {
+            if ($changedEvent.attr('name') !== 'all') {
+                disableConflictingEvents($changedEvent, $conflictingEvents)
+                changeTotal(100, $totalContainer);
+            } else {
+                changeTotal(200, $totalContainer);
+            }
+        } else {
             $conflictingEvents.prop("disabled", false);
-            $conflictingEvents.parent().css('color','#000')
+            $conflictingEvents.parent().css('color', '#000')
+            $changedEvent.attr('name') === 'all' ? changeTotal(-200, $totalContainer) : changeTotal(-100, $totalContainer);
         }
+
     }
 }
 
 function disableConflictingEvents($selectedEvent, $disableConflictingEvents) {
     $disableConflictingEvents.map((eventIndex) => {
         const $activity = $($disableConflictingEvents[eventIndex]);
-        if (!$activity.is($selectedEvent)){
+        if (!$activity.is($selectedEvent)) {
             $activity.prop('disabled', true);
-            $activity.parent().css({color: "#A9A9A9"});
+            $activity.parent().css({ color: "#A9A9A9" });
         } else {
             $activity.prop("disabled", false);
-        }  
+        }
     })
 }
 
+function initialiseTotalCounter() {
+    $totalContainer = $(`<div class = "Total"> Total: $ <span>0<span>  </div>`);
+
+    $totalContainer.hide();
+    $('.activities').append($totalContainer);
+
+    return $totalContainer;
+}
+
+function changeTotal(numberToChangeBy, $container) {
+    let $totalSpan = $container.find('span');
+    let total = parseInt($totalSpan.text());
+
+    total += numberToChangeBy;
+    $totalSpan.text(total);
+    runTotalAnimation(total, $totalSpan, $container);
+}
+
+function runTotalAnimation(total, $textSpan, $container) {
+    if (total === 0) {
+        $textSpan.fadeOut();
+        $container.slideUp(500);
+    } else {
+        $textSpan.fadeIn(600);
+        $container.slideDown(500);  
+    }
+}
+
+function handlePaymentSelection(){
+    const $paymentOptions = $('#payment');
+    const $creditCard = $('#credit-card');
+    const $otherPayments = $creditCard.siblings('div');
+   
+    setDefaultPaymentMethod($paymentOptions, $creditCard, $otherPayments);
+    
+    return function (event){
+        paymentSelection = $(event.target).val();
+        displaySelectedPaymentMethod(paymentSelection, $creditCard, $otherPayments);
+    }
+}
+
+function setDefaultPaymentMethod($options, $creditCard, $otherPayments){
+    $options.find("option[value = 'credit card']").prop('selected', true);
+    $options.find("option[value = 'select_method']").hide();
+    $creditCard.show();
+    $otherPayments.hide()
+}
+
+function displaySelectedPaymentMethod(selectedOption, $creditCard, $otherPayments){
+    if(selectedOption === 'credit card'){
+        $creditCard.show()
+        $otherPayments.hide();
+    } else {
+        $creditCard.hide();
+        
+        $otherPayments.map((index) => {
+            const chosenPayment = new RegExp(`${selectedOption}`, "i").test($($otherPayments[index]).find('p').text());
+            chosenPayment ? $($otherPayments[index]).show() : $($otherPayments[index]).hide();
+        });
+    }
+
+}
 
 
